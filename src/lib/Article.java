3ba5e36551035ab3fa1b20a402670b549699e7a1
ericminio <eric.mignot@gmail.com>
2014-05-11 12:01:06 -0400

    IntroduceField number to remove first MagicNumber

--- src/lib/Article.java
+++ src/lib/Article.java
@@ -2,9 +2,13 @@ package lib;
 
 public class Article {
 
-    public String text = "We've just discovered that 300 = 2 x 2 x 3 x 5 x 5!!!";
+    private Integer number;
+
+    public String text;
 
     public String toString() {
+        text = "We've just discovered that " + number + " = 2 x 2 x 3 x 5 x 5!!!";
+        
         return "An article with text = " + text;
     }
 
