c7c03f0ea13a4598e7bbed66753d6d934a77b911
ericminio <eric.mignot@gmail.com>
2014-05-11 09:30:55 -0400

    Green it with MagicNumber

--- src/lib/Article.java
+++ src/lib/Article.java
@@ -2,7 +2,7 @@ package lib;
 
 public class Article {
 
-    public String text;
+    public String text = "We've just discovered that 300 = 2 x 2 x 3 x 5 x 5!!!";
 
     public String toString() {
         return "An article with text = " + text;
